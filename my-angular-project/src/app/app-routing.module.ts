438058cac5a08c200ac8c0c4c7ddc702f52eea97
sa6656fa-s <sa6656fa-s@student.lu.se>
2022-02-28 11:31:23 +0100

    Implemented navigation to the different pages

--- my-angular-project/src/app/app-routing.module.ts
+++ my-angular-project/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MovieSearchComponent } from './movie-search/movie-search.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'sok-film', component: MovieSearchComponent },
+  { path: 'kontakt', component: ContactPageComponent},
+  { path: 'varukorg', component: ShoppingCartComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- my-angular-project/src/app/app.module.ts
+++ my-angular-project/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MovieSearchComponent } from './movie-search/movie-search.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieSearchComponent,
+    ContactPageComponent,
+    HomePageComponent,
+    ShoppingCartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
